954828aa6e2a8c48b8219cffd912a86fee342598
Jeremy Voegele <voegelejj@gmail.com>
2025-09-09 23:52:51 -0700

    clean up

--- resources/keys.py
+++ resources/keys.py
@@ -9,20 +9,20 @@ from dotenv import load_dotenv
 # ── Load Environment Variables ──────────────────────────────────────────
 load_dotenv(override=True)
 
-DEBUG              = os.getenv('DEBUG', 'False').lower() in ('true', '1', 't')
-LIVE_DISCORD_TOKEN = os.getenv('LIVE_DISCORD_TOKEN')
-BETA_DISCORD_TOKEN = os.getenv('BETA_DISCORD_TOKEN')
-OPENAI_API_KEY     = os.getenv('OPENAI_API_KEY')
-PLEX_URL           = os.getenv('PLEX_URL')
-PLEX_TOKEN         = os.getenv('PLEX_TOKEN')
-APP_URL            = os.getenv('APP_URL')
-SD_API_URL         = os.getenv('SD_API_URL')
-OLLAMA_IP          = os.getenv('OLLAMA_IP')
+DEBUG                   = os.getenv('DEBUG', 'False').lower() in ('true', '1', 't')
+LIVE_DISCORD_TOKEN      = os.getenv('LIVE_DISCORD_TOKEN')
+BETA_DISCORD_TOKEN      = os.getenv('BETA_DISCORD_TOKEN')
+OPENAI_API_KEY          = os.getenv('OPENAI_API_KEY')
+PLEX_URL                = os.getenv('PLEX_URL')
+PLEX_TOKEN              = os.getenv('PLEX_TOKEN')
+APP_URL                 = os.getenv('APP_URL')
+SD_API_URL              = os.getenv('SD_API_URL')
+OLLAMA_IP               = os.getenv('OLLAMA_IP')
 OLLAMA_LIVE_CHANNEL_ID  = int(os.getenv('OLLAMA_LIVE_CHANNEL_ID'))
 OLLAMA_BETA_CHANNEL_ID  = int(os.getenv('OLLAMA_BETA_CHANNEL_ID'))
-#OLLAMA_MODEL       = "phi3:mini"
-#OLLAMA_MODEL       = "llama3.2:3b"
-OLLAMA_MODEL       = "qwen2:1.5b"
+#OLLAMA_MODEL           = "phi3:mini"
+#OLLAMA_MODEL           = "llama3.2:3b"
+OLLAMA_MODEL            = "qwen2:1.5b"
 
 # ── Platform-Specific Overrides ─────────────────────────────────────────
 if DEBUG:
